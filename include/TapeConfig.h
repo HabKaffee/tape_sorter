6cf553611e8fc27a11ecc7022e9b7c8e2a03d001
Nikita Naumov <neknaum1@gmail.com>
2024-04-14 21:18:58 +0300

    Config file tested and fully working

--- include/TapeConfig.h
+++ include/TapeConfig.h
@@ -30,27 +30,32 @@ class TapeConfig {
               << " - " << pex.getError() << std::endl;
             return kFileParseError;
         }
-
-        const libconfig::Setting& root = config.getRoot();
-        const libconfig::Setting& tapeConfig = root["tapeConfig"];
-        if (!tapeConfig.lookupValue("writeDelay", this->writeDelay)) {
-            std::cerr << "\'writeDelay\' value not found in config file. Aborting..." << std::endl;
-            return kConfigValueNotFound;
-        }
-        if (!tapeConfig.lookupValue("readDelay", this->readDelay)) {
-            std::cerr << "\'readDelay\' value not found in config file. Aborting..." << std::endl;
-            return kConfigValueNotFound;
-        }
-        if (!tapeConfig.lookupValue("changePositionDelay", this->changePositionDelay)) {
-            std::cerr << "\'changePositionDelay\' value not found in config file. Aborting..." << std::endl;
-            return kConfigValueNotFound;
-        }
-        if (!tapeConfig.lookupValue("tapeScrollDelay", this->tapeScrollDelay)) {
-            std::cerr << "\'tapeScrollDelay\' value not found in config file. Aborting..." << std::endl;
-            return kConfigValueNotFound;
+        try {
+            const libconfig::Setting& root = config.getRoot();
+            const libconfig::Setting& tapeConfig = root["application"]["tapeConfig"];
+            if (!tapeConfig.lookupValue("writeDelay", this->writeDelay)) {
+                std::cerr << "\'writeDelay\' value not found in config file. Aborting..." << std::endl;
+                return kConfigValueNotFound;
+            }
+            if (!tapeConfig.lookupValue("readDelay", this->readDelay)) {
+                std::cerr << "\'readDelay\' value not found in config file. Aborting..." << std::endl;
+                return kConfigValueNotFound;
+            }
+            if (!tapeConfig.lookupValue("changePositionDelay", this->changePositionDelay)) {
+                std::cerr << "\'changePositionDelay\' value not found in config file. Aborting..." << std::endl;
+                return kConfigValueNotFound;
+            }
+            if (!tapeConfig.lookupValue("tapeScrollDelay", this->tapeScrollDelay)) {
+                std::cerr << "\'tapeScrollDelay\' value not found in config file. Aborting..." << std::endl;
+                return kConfigValueNotFound;
+            }
+        } catch (const libconfig::SettingNotFoundException &snfe) {
+            std::cerr << "Not found setting in config file" << std::endl;
+            return kFileParseError;
         }
         return kSuccess;
     }
+
     void printCurrentConfig() {
         std::cout << "Current configurtion:" << std::endl;
         std::cout << std::setw(22) << "writeDelay: " << this->writeDelay << std::endl
@@ -62,7 +67,7 @@ class TapeConfig {
  public:
     TapeConfig(std::string configPath) {
         std::uint8_t ret = readConfig(configPath);
-        if (ret) {
+        if (ret != kSuccess) {
             std::cerr << "Config parsing failed" << std::endl;
             std::exit(ret);
         }
